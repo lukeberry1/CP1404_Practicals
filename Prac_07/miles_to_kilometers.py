a941ce3683e5a2107553442bdd8bdecafc498bcd
Luke Berry <luke.berry@my.jcu.edu.au>
2019-10-09 12:15:07 +1000

    Prac_07 files

---
+++ Prac_07/miles_to_kilometers.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+
+
+
+class Convert_M_To_Km(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('miles_to_kilometers.kv')
+        return self.root
+
+    def handle_calculate(self):
+
+
+
+Convert_M_To_Km().run()
